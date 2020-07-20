ba10eedbb72b74bc3349efc55bd7b3c4b404e464
Reasno <guxi99@gmail.com>
2020-07-20 20:26:45 +0800

    feat: add axios interceptor

--- lib/tracing.module.ts
+++ lib/tracing.module.ts
@@ -1,18 +1,35 @@
+import {
+  DynamicModule,
+  HttpModule,
+  HttpService,
+  Module,
+  OnApplicationBootstrap,
+  Provider,
+} from '@nestjs/common';
+import { initTracer, TracingConfig, TracingOptions } from 'jaeger-client';
+import {
+  FORMAT_HTTP_HEADERS,
+  FORMAT_TEXT_MAP,
+  Span,
+  Tags,
+  Tracer,
+} from 'opentracing';
+import { TRACER } from './constant';
+import { TracingService } from './tracing.service';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import * as httpContext from 'express-http-context';
+import { ModuleRef } from '@nestjs/core';
 /**
  * Created by Rain on 2020/7/16
  */
-import { DynamicModule, Module, Provider } from '@nestjs/common';
-import { initTracer, TracingConfig, TracingOptions } from 'jaeger-client';
-import { Tracer } from 'opentracing';
-
-import { TRACER } from './constant';
-import { TracingService } from './tracing.service';
 
 @Module({
   providers: [TracingService],
   exports: [TracingService],
 })
-export class TracingModule {
+export class TracingModule implements OnApplicationBootstrap {
+  constructor(private moduleRef: ModuleRef) {}
+
   static tracer: Tracer;
 
   static forRoot(option: {
@@ -32,9 +49,76 @@ export class TracingModule {
     providers.push(tracerProvider);
 
     return {
+      imports: [HttpModule],
       module: TracingModule,
       providers,
-      exports: providers,
+      exports: [...providers, HttpModule],
     };
+  }
+
+  /**
+   * 设置axios拦截器，增加tracing信息
+   */
+  onApplicationBootstrap(): void {
+    this.moduleRef
+      .get<HttpService>(HttpService)
+      .axiosRef.interceptors.request.use(...this.interceptRequest());
+    this.moduleRef
+      .get<HttpService>(HttpService)
+      .axiosRef.interceptors.response.use(...this.interceptResponse());
+  }
+
+  private interceptRequest() {
+    return [
+      (config: AxiosRequestConfig) => {
+        const tracer = TracingModule.tracer;
+
+        let context = httpContext.get('tracingInfo');
+        if (!context) {
+          context = {};
+          httpContext.set('tracingInfo', context);
+        }
+
+        const ctx = tracer.extract(FORMAT_TEXT_MAP, context);
+
+        let span: Span;
+        if (ctx) {
+          span = tracer.startSpan('http', { childOf: ctx });
+        } else {
+          span = tracer.startSpan('http');
+        }
+        span.addTags({ url: config.url, method: config.method });
+
+        tracer.inject(span, FORMAT_HTTP_HEADERS, config.headers);
+
+        const spanConfig: any = config;
+        spanConfig.span = span;
+        return spanConfig as AxiosRequestConfig;
+      },
+      (error: any) => Promise.reject(error),
+    ];
+  }
+
+  private interceptResponse() {
+    return [
+      (response: AxiosResponse) => {
+        const config = response.config as AxiosRequestConfig & { span: Span };
+        config.span.log({
+          result: response.data,
+          statusCode: response.status,
+        });
+        config.span.finish();
+        return response;
+      },
+      (error: any) => {
+        const config = error.config as AxiosRequestConfig & { span: Span };
+        config.span.log({
+          result: error.response?.data,
+          statusCode: error.response?.status,
+        });
+        config.span.setTag(Tags.ERROR, true);
+        return Promise.reject(error);
+      },
+    ];
   }
 }
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,6 +1,28 @@
 /**
  * Created by Rain on 2020/7/20
  */
-describe('t', () => {
-  it('t', () => {});
+import { Test } from '@nestjs/testing';
+import { TracingModule } from '../lib';
+import Span from 'opentracing/lib/span';
+import { HttpService } from '@nestjs/common';
+
+describe('http module', () => {
+  let service: HttpService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        TracingModule.forRoot({
+          tracingOption: {},
+          tracingConfig: { serviceName: 'foo' },
+        }),
+      ],
+    }).compile();
+    service = moduleRef.get(HttpService);
+  });
+  it('should start tracing span', async () => {
+    const tracer = TracingModule.tracer;
+    jest.spyOn(tracer, 'startSpan');
+    const result = await service.get('http://jd.com', {}).toPromise();
+    expect(tracer.startSpan).toBeCalled;
+  });
 });
