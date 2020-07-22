054a1a85fc5b1643baa84e541bb629395ca34ebb
toonew <litaixin@donews.com>
2020-07-22 16:05:57 +0800

    feat(): add export content

--- lib/index.ts
+++ lib/index.ts
@@ -2,9 +2,14 @@
  * Created by Rain on 2020/7/20
  */
 export * from './decorator/spand.decorator';
+export * from './hook/index';
 export * from './interface/tracing.interface';
 
+export * from './constant';
 export * from './tracing-http.interceptor';
 export * from './tracing-grpc.interceptor';
 
 export * from './tracing.module';
+
+export * from 'opentracing';
+export * from 'jaeger-client';
